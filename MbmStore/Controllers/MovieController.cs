a46245cbbdc399bacbee48cffcd825bf6fbb9849
WodeCraft <jcr@wodecraft.dk>
2016-09-07 14:45:59 +0200

    Initial commit of solution

---
+++ MbmStore/Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MbmStore.Models;
+
+namespace MbmStore.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            
+            // create a new Movie object with instance name jungleBook
+            Movie jungleBook = new Movie("Jungle Book", 160.50m, "junglebook.jpg");
+
+            // assign a viewbag property to the new Movie object
+            ViewBag.JungleBook = jungleBook;
+
+
+            return View();
+        }
+    }
+}
---
+++ MbmStore/Models/Movie.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MbmStore.Models
+{
+    public class Movie
+    {
+        // fields
+        private string title; 
+        private decimal price; 
+        private string imageUrl;
+
+        // properties
+        public string Title
+        {
+            get { return title; } // read
+            set { title = value; } // write
+        }
+
+        public decimal Price 
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Price is not accepted");
+                }
+                else {
+                    price = value;
+                }
+            }
+            get { return price; }
+        }
+
+
+        public string ImageUrl 
+        {
+            get { return imageUrl; }
+            set { imageUrl = value; }
+        }
+
+
+        // constructors
+        public Movie(string title, decimal price)
+        {
+            this.title = title;
+            this.price = price;
+        }
+
+        public Movie(string title, decimal price, string imageUrl)
+        {
+            this.title = title;
+            this.price = price;
+            this.imageUrl = imageUrl;
+        }
+    }
+}
